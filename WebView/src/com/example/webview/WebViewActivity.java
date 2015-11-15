eb656d2e567712434ddc547d2fdbb2c9b05461bc
dvillela <dcvillela@gmail.com>
2015-11-15 17:20:04 -0200

    Versão I

---
+++ WebView/src/com/example/webview/WebViewActivity.java
@@ -0,0 +1,47 @@
+package com.example.webview;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class WebViewActivity extends Activity {
+
+	private WebView myWebView;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_web_view);
+		// WebView
+		this.myWebView = (WebView) findViewById(R.id.webview);
+		WebSettings webSettings = myWebView.getSettings();
+		webSettings.setJavaScriptEnabled(true);
+		myWebView.loadUrl("https://painel.locaweb.com.br/");
+		//myWebView.loadUrl("http://www.devmobilebrasil.com.br");
+		// Sistema Financeiro FEMA - DHCP de casa
+		//myWebView.loadUrl("http://192.168.0.148:8080/Financeiro/Login.xhtml");
+		myWebView.setWebViewClient(new HelloWebViewClient());
+	}
+
+	private class HelloWebViewClient extends WebViewClient {
+		@Override
+		public boolean shouldOverrideUrlLoading(WebView webView, String url) {
+			webView.loadUrl(url);
+			return true;
+		}
+	}
+	
+	@Override
+	public boolean onKeyDown(int keyCode, KeyEvent event){
+		if(keyCode == KeyEvent.KEYCODE_BACK && myWebView.canGoBack()){
+			myWebView.goBack();
+			return true;
+		}
+		return super.onKeyDown(keyCode, event);
+	}
+}
